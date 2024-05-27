eef8e5f772be2f9c5b28f49f99e3164ab2da1ff9
nyraareda <nyraareda3@gmail.com>
2024-05-27 19:47:51 +0300

    with adding authontication

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\Api;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use App\Models\ProductCategory;
