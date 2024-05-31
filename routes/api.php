aae52ed9fcc5bd40713c111d05ebf3d84d169506
nyraareda <nyraareda3@gmail.com>
2024-05-31 16:18:35 +0300

    edit in route

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::group(['middleware' => ['api', 'auth:api', 'role:admin']], function () {
     Route::get('/products', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::get('/products/{id}', [ProductController::class, 'show']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::post('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
     Route::post('/products/search', [ProductController::class, 'search']);
 
